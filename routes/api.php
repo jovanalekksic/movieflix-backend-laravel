e0f37d4968a4eea478e4f0ac199d3c010ec2fbd0
Jovana Aleksic <jaleksic23@gmail.com>
2023-05-08 22:45:12 +0200

    rute za API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserMovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/movies/{id}', [MovieController::class, 'show']);
+// Route::get('/movies', [MovieController::class, 'index']);
+
+Route::resource('movies', MovieController::class);
+Route::resource('users.posts', UserMovieController::class);
+
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{id}/movies', [UserMovieController::class, 'index'])->name('users.movies.index');
