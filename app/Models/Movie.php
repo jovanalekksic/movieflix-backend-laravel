e2573cec6545a5e6f823a75b928905d54b00c587
Jovana Aleksic <jaleksic23@gmail.com>
2023-05-08 14:44:07 +0200

    definisanje veza izmedju tabela

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        'title',
+        'description',
+        'picture',
+        'rating',
+        'studio_id',
+        'user_id',
+        'genre_id',
+        'name',
+
+    ];
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+    public function studio()
+    {
+        return $this->belongsTo(Studio::class);
+    }
+    public function user()
+    {
+        return $this->hasMany(User::class, 'user_id');
+    }
 }
--- app/Models/Studio.php
+++ app/Models/Studio.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Studio extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+    ];
+
+    public function movies()
+    {
+        return $this->hasMany(Movie::class);
+    }
 }
